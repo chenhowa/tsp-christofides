0583e913c6b17c80a05c64e926afa5ad602bcfe1
chenhowa <chenhowa@oregonstate.edu>
2017-08-15 18:56:33 -0700

    Updated christofides_2.py with documentation and perfect matching code

--- christofides_2.py
+++ christofides_2.py
@@ -2,37 +2,59 @@ import math
 import os
 import sys
 
+
+#Represents a city.
+# Has three attributes
+#   [1] name, an integer identifer
+#   [2] x, the integer x-position
+#   [3] y, the integer y-position
 class City:
 	def __init__(self, name, x, y):
 		self.name = name
 		self.x = x
 		self.y = y
 
+#Represents an edge
+# has three attributes
+#   [1] u, the name of one City of the Edge.
+#   [2] v, the name of the other City of the Edge
 class Edge:
 	def __init__(self, city1, city2):
 		self.u = city1.name
 		self.v = city2.name
 		self.w = self.int_distance(city1, city2)
 
+        # Calculates Euclidean distance between 2 Cities based on their positions
 	def int_distance(self, city1, city2):
 		dist = (city1.x - city2.x)**2 + (city1.y - city2.y)**2
 		dist = int(round(math.sqrt(dist)))
 		return dist
 
-
+#Represents a Graph by storing all the Edges of that graph
+# has 2 attributes
+#   [1] V, the number of vertices in the graph
+#   [2] graph, a list of edge properties that make up the Graph
 class Graph:
 	def __init__(self, vertices):
 		self.V = vertices
 		self.graph = []
 
+        # Adds a list of edge properties to the list of edges:
+        #   edge.u: id of one city
+        #   edge.v: id of another city
+        #   edge.w: DISTANCE between cities
 	def addEdge(self, edge):
 		self.graph.append([edge.u, edge.v, edge.w])
 
+        #Finds the name (ID) of something??
+        # Use: MST only
 	def find(self, parent, i):
 		if parent[i] == i:
 			return i
 		return self.find(parent, parent[i])
 
+        # Merges two Graphs on the condition that they are TREES
+        # Use: MST only
 	def union(self, parent, rank, x, y):
 		root_x = self.find(parent, x)
 		root_y = self.find(parent, y)
@@ -44,15 +66,20 @@ class Graph:
 			parent[root_y] = root_x
 			rank[root_x] += 1
 
+        # Uses Kruskal's algorithm to find an MST of the Graph (self)
+        # Returned as a list of edges [u, v, w]
 	def MST(self):
 		res = []
 		idx = 0
-		edge = 0
-		parent = []
+		edge = 0   #Keeps track of the edges we are processing
+		parent = [] 
 		rank = []
 
+                # Sorts the list of edges by weight
 		self.graph = sorted(self.graph, key=lambda x:x[2])
 
+                #Hard for me to tell if the remainder is correctly
+                # implementing Kruskal's algorithm (which is complex anyways
 		for node in range(self.V):
 			parent.append(node)
 			rank.append(0)
@@ -71,12 +98,14 @@ class Graph:
 
 
 # Takes as input an MST (Graph class) and V = number of vertices
-# Returns indices of odd degree vertices
+# Returns a list of IDs of vertices with odd degree
 def _odd_vertices(MST, V):
 	odd_vertices = [0 for _ in range(V)]
+        #Create a list of the degrees of each vertex
 	for u,v,w in MST:
 		odd_vertices[u] = odd_vertices[u]+1
 		odd_vertices[v] = odd_vertices[v]+1
+        #Filter out the indices of even degree
 	odd_vertices = [vertex for vertex, degree in enumerate(odd_vertices) if degree%2 == 1]
 	return odd_vertices
 
@@ -92,7 +121,39 @@ def nearest_neighbors(odd_cities):
 		unvisited.remove(next_city)
 	return visited
 
+#Takes as (hopefully) a list of Cities that make up a tour
+# Returns the perfect matching edge set
+# There MUST be an even number of odd-degree vertices in a graph.
+def compute_pm_weight(tour, num_vertices):
+    # This algorithm fails for small num_vertices
 
+    #Calculate the weight of the first "perfect matching"
+    perfect_match_1 = []
+    M1 = 0
+    i = 0
+    while i < (num_vertices / 2):
+        M1 = M1 + euc_dist(tour[i], tour[i+2])
+        i = i + 4
+        perfect_match_1.append(Edge( tour[i], tour[i+2] )
+
+    #Calculate the weight of the second "perfect matching"
+    # TOO BAD THE PAPER DESCRIPTION SUCKS DONKEY @@@@
+    M2 = 0
+    i = 1
+    while i < ((num_vertices / 2) - 1):
+        M2 = M2 + euc_distance(tour[i], tour[i+2])
+        i = i + 4
+    M2 = M2 + euc_distance(tour[0], tour[num_Vertices - 1]
+
+    if M1 < M2:
+        i = 0
+    else:
+
+
+def euc_dist(city1, city2):
+        dist = (city1.x - city2.x)**2 + (city1.y - city2.y)**2
+        dist = int(round(math.sqrt(dist)))
+        return dist
 
 def main():
 
@@ -114,18 +175,23 @@ def main():
 	cities = []
 	for line in fread:
 		data = line.split()
+                # turn all the data into integers
 		data = [int(val) for val in data]
+                #           id      x-pos       y-pos
 		curr = City(data[0], data[1], data[2])
+                #Add to the list of cities
 		cities.append(curr)
 
+        #At this point we have a complete list of cities
+        #Time to build edges, 1 edge per pair of cities
 	edges = []
 	for i in range(len(cities)-1):
 		for j in range(i+1, len(cities)):
 			edge = Edge(cities[i], cities[j])
 			edges.append(edge)
 
-	
-	# BEGIN CHRISTOFIDES ALGORITHM
+        #Now we have a list of cities and a list of all possible edges between them.	
+	# We fuse these two lists into one data structure, a Graph
 	G = Graph(len(cities))
 	for edge in edges:
 		G.addEdge(edge)
